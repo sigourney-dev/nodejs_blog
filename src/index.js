c9c60de0fe47fa394b7b1d45c160af7db7f4ad69
vunh <vunh@nobisoft.com.vn>
2024-01-18 11:17:04 +0700

    update controller

--- src/index.js
+++ src/index.js
@@ -5,22 +5,27 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
-app.use(morgan('combined'));
+// HTTP logger
+// app.use(morgan('combined'));
 
-app.engine('hbs', handlebars.engine({default: 'main', extname: '.hbs'}));
+// Template engine
+app.engine('hbs', handlebars.engine({ default: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+// Router init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
